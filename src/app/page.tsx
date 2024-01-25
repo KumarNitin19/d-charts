181f5da5fdf4fc17c924a32a1de4b739ed15dcb9
Nitin Kumar <nitinkumardeshmukh19@gmail.com>
2024-01-25 13:06:24 +0530

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,125 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect } from "react";
+import * as d3 from "d3";
 import styles from "./page.module.css";
 
 export default function Home() {
+  let width = 600,
+    height = 400;
+
+  const draw = () => {
+    let colorScale = ["orange", "lightblue", "#B19CD9"];
+    let xCenter = [100, 300, 500];
+    let numNodes = 100;
+    let nodes = d3.range(numNodes).map(function (d, i) {
+      return {
+        radius: Math.random() * 25,
+        category: i % 3,
+        value: Math.random(),
+      };
+    });
+
+    var forceStrength = 0.2;
+    var center = { x: width / 2, y: height / 2 };
+
+    function charge(d) {
+      return -Math.pow(d.radius, 2.0) * forceStrength;
+    }
+
+    // simulation.stop();
+
+    d3.select(".tabs")
+      .selectAll(".tab")
+      .on("click", function (e) {
+        simulate(e.target.id);
+      });
+
+    simulate("1");
+
+    function simulate(type) {
+      let simulation = d3
+        .forceSimulation(nodes)
+        .velocityDecay(0.5)
+        .force("x", d3.forceX().strength(forceStrength).x(center.x))
+        .force("y", d3.forceY().strength(forceStrength).y(center.y))
+        .force("charge", d3.forceManyBody().strength(charge))
+        .force(
+          "collision",
+          d3.forceCollide().radius(function (d) {
+            return d.radius;
+          })
+        )
+        .on("tick", ticked);
+      if (type === "1") {
+        simulation.force("center", d3.forceCenter(width / 2, height / 2));
+      } else if (type === "2") {
+        simulation.force(
+          "x",
+          d3.forceX().x(function (d) {
+            return xCenter[d.category];
+          })
+        );
+      } else {
+        // appendScale();
+        let xScale = d3.scaleLinear().domain([0, 1]).range([0, 600]);
+        simulation
+          .force(
+            "x",
+            d3.forceX().x(function (d) {
+              return xScale(d.value);
+            })
+          )
+          .force(
+            "y",
+            d3.forceY().y(function (d) {
+              return 0;
+            })
+          );
+      }
+    }
+
+    function ticked() {
+      let u = d3
+        .select("svg g")
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("r", function (d) {
+          return d.radius;
+        })
+        .style("fill", function (d) {
+          return colorScale[d.category];
+        })
+        .attr("cx", function (d) {
+          return d.x;
+        })
+        .attr("cy", function (d) {
+          return d.y;
+        });
+    }
+  };
+
+  useEffect(() => {
+    draw();
+  }, []);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div id="content">
+        <div className="tabs">
+          <button id="1" className="tab">
+            State 1
+          </button>
+          <button id="2" className="tab">
+            State 2
+          </button>
+          <button id="3" className="tab">
+            State 3
+          </button>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <svg width="700" height="600">
+          <g transform="translate(50, 200)"></g>
+        </svg>
       </div>
     </main>
   );

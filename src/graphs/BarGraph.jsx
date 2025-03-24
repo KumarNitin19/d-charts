c365c5ef82f54d4c167efba9f98cd55622e377e6
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-03-25 00:00:45 +0530

    feat: bar chart lower axis added

--- src/graphs/BarGraph.jsx
+++ src/graphs/BarGraph.jsx
@@ -1,18 +1,38 @@
 import { useEffect } from "react";
+import * as d3 from "d3";
 
 export default function BarChart() {
-  const height = 500,
-    width = 960,
-    margin = 40;
+  const margin = { top: 40, right: 40, bottom: 80, left: 80 },
+    height = 500 - margin.top - margin.bottom,
+    width = 960 - margin.left - margin.right;
 
   const drawBarChart = () => {
     const svg = d3
       .select("#bar-chart")
       .append("svg")
-      .attr("height", height)
-      .attr("width", width)
+      .attr("height", height + margin.top + margin.bottom)
+      .attr("width", width + margin.left + margin.right)
       .append("g")
-      .attr("transform", `translate(${margin}, ${margin})`);
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    d3.csv(
+      "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv"
+    ).then(function (data) {
+      // X axis
+      const x = d3
+        .scaleBand()
+        .range([0, width])
+        .domain(data.map((d) => d.Country))
+        .padding(0.2);
+
+      svg
+        .append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .attr("text-anchor", "end");
+    });
   };
 
   useEffect(() => {
